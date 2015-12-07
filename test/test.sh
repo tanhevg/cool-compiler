48fa00a96d42c5e40e39641f275fbb162067fd55
Evgeny Tanhilevich <evgeny.tanhilevich@gmail.com>
2015-12-07 17:05:31 +0000

    more test cases

--- test/test.sh
+++ test/test.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
-function test {
+function testex {
     ../output/Debug/cool-parser -o ../output/$1.tree ../../cool/cool-examples/$1.cl  > ../output/$1.out 2>../output/$1.err
 }
-#test arith
-#test cells
-#test hello-world
-#test hs
-test list
-test new-complex
-test primes
-test print-cool
-test sort-list.cl
+
+function test {
+    ../output/Debug/cool-parser -o ../output/$1.tree $1.cl  > ../output/$1.out 2>../output/$1.err
+}
+
+testex arith
+testex cells
+testex hello-world
+testex hs
+testex list
+testex new-complex
+testex primes
+testex print-cool
+testex sort-list.cl
+test good_semant
+test bad_semant
