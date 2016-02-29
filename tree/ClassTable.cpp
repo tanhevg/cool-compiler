64a1b33abb96caadd45f0430daa81ca1f0a8f2a3
Evgeny Tanhilevich <evgeny.tanhilevich@gmail.com>
2016-02-29 23:29:09 +0100

    emit prototype objects; fix bugs in type checker

---
+++ tree/ClassTable.cpp
@@ -0,0 +1,170 @@
+//
+// Created by Evgeny Tanhilevich on 04/01/2016.
+//
+
+#include "ClassTable.h"
+#include "extern_symbols.h"
+
+
+Class_ ClassTable::get_class(Symbol s) {
+    auto find = class_by_name.find(s);
+    if (find == class_by_name.end()) {
+        return nullptr;
+    }
+    return find->second->get_class();
+}
+
+Symbol ClassTable::get_parent(Symbol class_name) {
+    return get_class(class_name)->get_parent();
+}
+
+Symbol ClassTable::join(const vector<Symbol> &types) {
+    vector<vector<Symbol>> ancestors;
+    for(Symbol s : types) {
+        vector<Symbol> v;
+        Symbol ss = s;
+        while(ss != No_class) {
+            v.insert(v.begin(), ss);
+            ss = get_class(ss)->get_parent();
+        }
+        // although vector push_back accepts parameters by reference, it creates copies of values for storing them
+        // use std::move to create an rvalue reference, such that move is performed instead of a copy
+        ancestors.push_back(move(v));
+    }
+    int i = 0;
+    bool br = false;
+    Symbol ret = nullptr, test;
+    vector<Symbol>& v0 = ancestors[0];
+    do {
+        test = v0[i];
+        for(vector<Symbol>& v : ancestors) {
+            if (i >= v.size() || v[i] != test) {
+                br = true;
+                break;
+            }
+        }
+        if (!br) {
+            ++i;
+            ret = test;
+        }
+    } while (!br && i < v0.size());
+    return ret;
+}
+
+bool ClassTable::is_subtype(Symbol sub, Symbol super) {
+    if (sub == No_type) {
+        return true;
+    }
+    while (sub != No_class) {
+        if (sub == super) {
+            return true;
+        }
+        sub = get_class(sub)->get_parent();
+    }
+    return false;
+}
+
+AttributeRecord* ClassTableRecord::get_attribute_record(Symbol s) {
+    auto it = attribute_by_name.find(s);
+    if (it == attribute_by_name.end()) {
+        return nullptr;
+    } else {
+        return it->second;
+    }
+}
+
+int ClassTable::get_attr_index(Symbol class_name, Symbol attr_name)
+{
+    do {
+        AttributeRecord *attribute_record = class_by_name[class_name]->get_attribute_record(attr_name);
+        if (attribute_record != nullptr) {
+            return attribute_record->get_index();
+        }
+        class_name = get_parent(class_name);
+    } while (class_name != No_class);
+    return -1;
+}
+void ClassTable::before(class__class *node)
+{
+    class_by_name[node->get_name()] = currentClassRecord = new ClassTableRecord(node, class_tag++);
+}
+
+void ClassTable::after(attr_class *node)
+{
+    currentClassRecord->add_attr(node);
+}
+
+ClassTableRecord::~ClassTableRecord() {
+    for (auto item : attribute_by_name) {
+        delete item.second;
+    }
+}
+
+ClassTable::~ClassTable() {
+    for (auto item : class_by_name) {
+        delete item.second;
+    }
+}
+
+void ClassTableRecord::add_attr(attr_class *attr) {
+    AttributeRecord *record = new AttributeRecord(attr);
+    attribute_by_name[attr->get_name()] = record;
+}
+
+int ClassTable::get_prototype_size(Symbol class_name) {
+    return class_by_name[class_name]->get_prototype_size();
+}
+
+void ClassTable::visit_attrs_of_class(Symbol class_name, TreeVisitor *other) {
+    class_by_name[class_name]->visit_attributes(other);
+
+}
+
+void ClassTableRecord::visit_attributes(TreeVisitor *visitor) {
+    for (auto item : attribute_by_name) {
+        Feature attr = item.second->get_attr();
+        attr->traverse_tree(visitor);
+    }
+}
+
+void ClassTableRecord::index_attributes(int start_idx) {
+    for (auto item : attribute_by_name) {
+        item.second->set_index(start_idx++);
+    }
+    indexed = true;
+}
+
+void ClassTable::index_attributes() {
+    for (auto item : class_by_name) {
+        index_attributes_rec(item.first);
+    }
+}
+
+void ClassTableRecord::copy_attributes(ClassTableRecord *parent_record) {
+    for (auto attr : parent_record->attribute_by_name) {
+        attribute_by_name[attr.first] = attr.second;
+    }
+}
+
+void ClassTable::index_attributes_rec(Symbol class_name) {
+    ClassTableRecord *&class_table_record = class_by_name[class_name];
+    if (class_table_record->is_indexed()) {
+        return;
+    }
+    Symbol parent_name = class_table_record->get_class()->get_parent();
+    if (parent_name != No_class) {
+        index_attributes_rec(parent_name);
+        ClassTableRecord *&parent_record = class_by_name[parent_name];
+        int start_index = parent_record->get_prototype_size();
+        class_table_record->index_attributes(start_index);
+        class_table_record->copy_attributes(parent_record);
+    } else {
+        class_table_record->index_attributes(0);
+    }
+
+
+}
+
+int ClassTable::get_class_tag(Symbol class_name) {
+    return class_by_name[class_name]->get_class_tag();
+}
---
+++ tree/ClassTable.h
@@ -0,0 +1,98 @@
+//
+// Created by Evgeny Tanhilevich on 04/01/2016.
+//
+
+#ifndef COOL_COMPILER_CLASSTABLE_H
+#define COOL_COMPILER_CLASSTABLE_H
+
+#include <map>
+#include <vector>
+#include "visitor.h"
+
+using std::map;
+using std::vector;
+
+class class__class;
+class attr_class;
+
+class AttributeRecord {
+private:
+    attr_class *attr;
+    int index;
+    friend class ClassTable;
+public:
+    AttributeRecord(attr_class * _attr): attr(_attr), index(-1) {}
+    attr_class* get_attr() {
+        return attr;
+    }
+    int get_index() {
+        return index;
+    }
+    void set_index(int _index) {
+        index = _index;
+    }
+};
+
+class ClassTableRecord {
+private:
+    Class_ cls;
+    int prototype_size;
+    map<Symbol, AttributeRecord*> attribute_by_name;//todo use rvalue reference?
+    int class_tag;
+    bool indexed;
+public:
+    ClassTableRecord(Class_ _cls, int _class_tag):
+            attribute_by_name(), cls(_cls), class_tag(_class_tag), indexed(false) {}
+    int get_attribute_count() {
+        return (int) attribute_by_name.size();
+    }
+    int get_prototype_size() {
+        return (int) (attribute_by_name.size() + 3); // space for class tag, object size and dispatch pointer
+    }
+    Class_ get_class() {
+        return cls;
+    }
+    int get_class_tag() {
+        return class_tag;
+    }
+    bool is_indexed() {
+        return indexed;
+    }
+    AttributeRecord* get_attribute_record(Symbol s);
+    void add_attr(attr_class *attr);
+    void visit_attributes(TreeVisitor *visitor);
+    void index_attributes(int start_idx);
+    virtual ~ClassTableRecord();
+
+    void copy_attributes(ClassTableRecord *parent);
+};
+
+class ClassTable : public TreeVisitor {
+    map<Symbol, ClassTableRecord*> class_by_name;
+    ClassTableRecord* currentClassRecord;
+    void index_attributes_rec(Symbol class_name);
+    int class_tag;
+public:
+    virtual ~ClassTable();
+    ClassTable() : class_by_name(), currentClassRecord(nullptr), class_tag(0) {}
+    Class_ get_class(Symbol s);
+    Symbol get_parent(Symbol class_name);
+    bool is_subtype(Symbol sub, Symbol super);
+    int get_attr_index(Symbol class_name, Symbol attr_name);
+    int get_prototype_size(Symbol class_name);
+
+    Symbol join(const vector<Symbol> &type1);
+
+    void before(class__class *node);
+    void after(attr_class *node);
+
+    void visit_attrs_of_class(Symbol class_name, TreeVisitor *other);
+
+    void index_attributes();
+
+    int get_class_tag(Symbol class_name);
+
+};
+
+
+#endif //COOL_COMPILER_CLASSTABLE_H
